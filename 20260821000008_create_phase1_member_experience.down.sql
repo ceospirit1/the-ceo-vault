4b3998e0fffd014dfa70355d4d9caf0301d4fb07
ceospirit1 <jeremy_davis1989@live.com>
2026-08-22 10:10:59 -0400

    Delete 20260821000008_create_phase1_member_experience.down.sql

--- 20260821000008_create_phase1_member_experience.down.sql
+++
@@ -1,8 +0,0 @@
--- Rollback: 008_create_phase1_member_experience.down.sql
-DROP FUNCTION IF EXISTS public.get_member_entitlement_status(UUID) CASCADE;
-DROP FUNCTION IF EXISTS public.claim_founder_spot(UUID, TEXT) CASCADE;
-DROP TABLE IF EXISTS public.user_saved_reports CASCADE;
-DROP TABLE IF EXISTS public.vault_folders CASCADE;
-DROP TABLE IF EXISTS public.member_gamer_profiles CASCADE;
-DROP TABLE IF EXISTS public.founder_allocations CASCADE;
-DROP TABLE IF EXISTS public.member_entitlements CASCADE;
