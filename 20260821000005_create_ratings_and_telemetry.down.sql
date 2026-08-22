6be1780d2f98fdf9523ad644bc38ac733dbd738f
ceospirit1 <jeremy_davis1989@live.com>
2026-08-22 10:10:21 -0400

    Delete 20260821000005_create_ratings_and_telemetry.down.sql

--- 20260821000005_create_ratings_and_telemetry.down.sql
+++
@@ -1,5 +0,0 @@
--- Rollback: 005_create_ratings_and_telemetry.down.sql
-DROP TABLE IF EXISTS feature_usage_daily CASCADE;
-DROP TABLE IF EXISTS search_telemetry CASCADE;
-DROP TABLE IF EXISTS build_ratings CASCADE;
-DROP TABLE IF EXISTS jumpshot_ratings CASCADE;
