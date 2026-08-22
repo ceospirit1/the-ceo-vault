ea8e34f8fbb4d18ebc454d9ec8622ee0cd61a4fd
ceospirit1 <jeremy_davis1989@live.com>
2026-08-22 10:10:00 -0400

    Delete 20260821000003_create_meta_catalog.down.sql

--- 20260821000003_create_meta_catalog.down.sql
+++
@@ -1,5 +0,0 @@
--- Rollback: 003_create_meta_catalog.down.sql
-DROP TABLE IF EXISTS jumpshot_recipes CASCADE;
-DROP TABLE IF EXISTS jumpshot_components CASCADE;
-DROP TABLE IF EXISTS animations CASCADE;
-DROP TABLE IF EXISTS badge_requirements CASCADE;
